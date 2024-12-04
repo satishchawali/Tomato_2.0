d0ece930bd5fc59faef63d7f0f4c2298d6f44390
satishchawali <148488004+satishchawali@users.noreply.github.com>
2024-12-05 01:18:48 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,22 +1,36 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../FoodItem/FoodItem'
-const FoodDisplay = ({category}) => {
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
-    const {food_list} = useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+
+  if (!food_list || food_list.length === 0) {
+    return <p>No food items available</p>;
+  }
+
+  const filteredFoodList = food_list.filter(
+    (item) => category === "All" || category === item.category
+  );
+
   return (
-    <div className='food-display' id='food-display'>
+    <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item,index)=>{
-          if(category==="All" || category === item.category){
-            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-          }
-        })}
+        {filteredFoodList.map((item) => (
+          <FoodItem 
+            key={item._id} 
+            id={item._id} 
+            name={item.name} 
+            description={item.description} 
+            price={item.price} 
+            image={item.image} 
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
