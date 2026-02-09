001cb6c4826e258c2665f48f3051975c8d19d599
Satish Chawali <satishchawali45@gmail.com>
2026-02-09 23:31:49 +0530

    built the new version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory
+    outDir: 'dist', // Specify the output directory
   },
 });
